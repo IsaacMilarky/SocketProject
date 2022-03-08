f79e3dd49d54f6af5081fd20297387f1a7a24201
Isaac Milarsky <krabs@tilde.team>
2022-03-08 12:46:06 -0600

    Define Standard server tcp connection
    Signed-off-by: Isaac Milarsky <krabs@tilde.team>

---
+++ server.cpp
@@ -0,0 +1,17 @@
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+
+int main()
+{
+    try
+    {
+        //Get object to represent io features of curent OS
+        boost::asio::io_context io_context;
+
+        //Object to listen for new connections
+    }
+}
