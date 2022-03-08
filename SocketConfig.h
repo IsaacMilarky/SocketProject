493664001b936a1c26a1260900c1ef8c116d38d5
Isaac Milarsky <krabs@tilde.team>
2022-03-08 11:01:22 -0600

    init commit

---
+++ SocketConfig.h
@@ -0,0 +1,2 @@
+#define Split_VERSION_MAJOR 
+#define Split_VERSION_MINOR 
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <boost/asio.hpp>
+
+int main(int argc, char ** argv)
+{
+    boost::asio::io_context io;
+
+    boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
+
+    t.wait();
+
+    std::cout << "Hello, world!" << std::endl;
+    
+    return 0;
+}
