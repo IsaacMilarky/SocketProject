d2ffddeed3be00a7aeee69d809f72e9080bebdbf
Isaac M <krabs@tilde.team>
2022-03-12 14:30:30 -0600

    progress
    Signed-off-by: Isaac M <krabs@tilde.team>

--- client.cpp
+++ client.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
+#include "include/Client/TCPClient.hpp"
 
 int main()
 {
     std::cout << "My chat room client. Version one." << std::endl;
-    
+
+    TCPClient client("127.0.0.1",14860);
+
+    std::string userInput = "";
+
+    while(userInput != "logout")
+    {
+        std::cout << ">";
+        std::cin >> userInput;
+        std::cout << std::endl;
+
+        
+    }
+
     return 0;
 }
